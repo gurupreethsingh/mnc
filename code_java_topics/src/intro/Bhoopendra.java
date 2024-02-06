e63427ad8b61e1caa89d68efb72ff98f7799d915
bhoopendras982 <bhoopendras982@gmail.com>
2024-02-06 11:56:05 +0530

    new file added

---
+++ code_java_topics/src/intro/Bhoopendra.java
@@ -0,0 +1,7 @@
+package intro;
+
+public class Bhoopendra {
+    public static void main(String args[]){
+        System.out.println("Ecoders");
+    }
+}
--- code_java_topics/src/intro/IntroductionJava.java
+++ code_java_topics/src/intro/IntroductionJava.java
@@ -2,8 +2,7 @@ package intro;
 
 public class IntroductionJava {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.print("Hello MNC to automation testing course");
 
 	}
