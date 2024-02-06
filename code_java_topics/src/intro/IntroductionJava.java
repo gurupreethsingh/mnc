08d08186ecd701eef3875ad883c71abe44bc7ffe
gurupreethsingh <igurupreeth@gmail.com>
2024-02-06 11:51:24 +0530

    second line added hello programmers

--- code_java_topics/src/intro/IntroductionJava.java
+++ code_java_topics/src/intro/IntroductionJava.java
@@ -4,7 +4,9 @@ public class IntroductionJava {
 
 	public static void main(String[] args) 
 	{
-		System.out.print("Hello MNC to automation testing course");
+		System.out.println("Hello MNC to automation testing course");
+		
+		System.out.println("hello programers");
 
 	}
 
