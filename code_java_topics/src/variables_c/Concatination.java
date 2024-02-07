97ec6d14f6566b597bbf14bef74b092f588a53e8
omega525 <chitti5250@gmail.com>
2024-02-07 12:39:29 +0530

    adding variables, concatination

---
+++ code_java_topics/src/variables_c/Concatination.java
@@ -0,0 +1,33 @@
+package variables_c;
+
+public class Concatination {
+	
+	public static void main(String[] args)
+	{
+		System.out.println(3);
+		   System.out.println(3.1995);	
+		   System.out.println('a');
+		   System.out.println('3');
+		   System.out.println("chetan");
+		   System.out.println("Nutan");
+		   System.out.println(true);
+		   System.out.println(false);
+		   
+		   
+		   byte a = 10;
+		   short b = 200;
+		   char c = 'c';
+		   int d = 2000;
+		   float e = 7.2000f;
+		   long f = 211111;
+		   double g = 12.3213322;
+		   System.out.println(a);	
+		   System.out.println(b);
+		   System.out.println(c);
+		   System.out.println(d);
+		   System.out.println(e);
+		   System.out.println(f);
+		   System.out.println(g);
+		   
+	}
+}
