ad634074afa3cdeae2e2687ec7246ac44b634b40
nutanhawaldar <nutanhawaldar@gmail.com>
2024-02-07 12:52:17 +0530

    variables & concatination

---
+++ code_java_topics/src/variables_n/Variables.java
@@ -0,0 +1,53 @@
+ package variables_n;
+ 
+ public class Variables
+
+{
+public static void main(String[] args)
+{
+	//data typesand variables
+	System.out.println(10);
+	System.out.println(2.35);
+	
+	//charecter literals, single quotes.
+	System.out.println('a');
+	System.out.println('9');
+	
+	//string literals
+	System.out.println("hello");
+	System.out.println("9036985520");
+	System.out.println("nutan7898");
+	System.out.println("     ");
+	
+	//boolean
+	System.out.println(true);
+	System.out.println(false);
+	
+	//8 data types
+	//declering variables
+	
+	byte a1=1;
+	short s=2;
+	char ch='a';
+	int a= 100;
+	float f=2.35f;
+	long l =12345678;
+	double d= 87654321;
+	
+	//string,inbuilt class in java.
+	String s1="chetan";
+	String s2="nutan";
+	String s3="gurupreet";
+	String username= "chetan";
+	System.out.println(a1);
+	System.out.println(s);
+	System.out.println(ch);
+	System.out.println(a);
+	System.out.println(f);
+	System.out.println(l);
+	System.out.println(s1);
+	System.out.println(s2);
+	System.out.println(s3);
+	System.out.println(username);
+  }
+}
