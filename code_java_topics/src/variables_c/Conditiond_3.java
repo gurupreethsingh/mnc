66ca8988d7d44af0a360e021dccf2106ea7d836b
omega525 <chitti5250@gmail.com>
2024-02-08 13:51:38 +0530

    Added condition_3 codes

---
+++ code_java_topics/src/variables_c/Conditiond_3.java
@@ -0,0 +1,15 @@
+package variables_c;
+
+public class Conditiond_3 {
+
+	public static void main(String[] args)
+	{
+	int b = 7;
+		for (int a=1; a<=10; a++)
+		{
+			System.out.println(a*b);
+		}
+
+	}
+
+}
