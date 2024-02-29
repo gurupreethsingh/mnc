69d6da0b66eb85621b815c551f1f3870da2752f1
omega525 <chitti5250@gmail.com>
2024-02-29 13:12:01 +0530

    new code added

---
+++ code_java_topics/src/variables_c/Sample_16.java
@@ -0,0 +1,14 @@
+package variables_c;
+
+public class Sample_16 {
+
+	public static void main(String[] args) {
+	int i=(a+b)==10;
+	int j=(b+c)==20;
+	int k=(a+c)==24;
+	
+	System.out.println(a+b+c);
+
+	}
+
+}
